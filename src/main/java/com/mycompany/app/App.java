009387770b56be93e82b132056be09ed9033d19a
mani-demo <38170649+mani-demo@users.noreply.github.com>
2018-04-14 03:06:18 +0530

    test on triggers

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! Jenkins build success";
+    private final String message = "Hello World! Jenkins build success test on trigger";
 
     public App() {}
 
