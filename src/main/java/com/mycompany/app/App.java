2b5cdaeb9bd2c1d5ae2570d5f5d5f5924c6f18bf
mani-demo <38170649+mani-demo@users.noreply.github.com>
2018-09-04 08:58:31 +0530

    commit last

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,7 +7,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World.. S3 integration test!";
+    private final String message = "Hello World";
 
     public App() {}
 
