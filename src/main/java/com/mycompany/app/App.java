1aadf41af957b8d4b9ff8cd2408df47fac074e14
mani-demo <38170649+mani-demo@users.noreply.github.com>
2018-09-05 08:31:07 +0530

    updated access token

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,7 +7,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! github";
+    private final String message = "Hello World! github access token added";
 
     public App() {}
 
